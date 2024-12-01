57a49f012f4fb9c5fdba38187fac8829cef7c09c
Anderson Souza <andersonssilveira96@gmail.com>
2024-12-01 14:15:15 -0300

    Adding new yaml files

--- src/kubernetes/setup.ps1
+++ src/kubernetes/setup.ps1
@@ -1,8 +1,24 @@
 # Metrics para obter informações e fazer o HPA da API.
 kubectl apply -f ./src/kubernetes/metrics-server.yaml
 
-# API
-kubectl apply -f ./src/kubernetes/api-secret.yaml
-kubectl apply -f ./src/kubernetes/api-deployment.yaml
-kubectl apply -f ./src/kubernetes/api-svc.yaml
-kubectl apply -f ./src/kubernetes/api-hpa.yaml
+# RabbitMQ
+kubectl apply -f ./src/kubernetes/rabbitmq-deployment.yaml
+kubectl apply -f ./src/kubernetes/rabbitmq-service.yaml
+
+# API Pedido
+kubectl apply -f ./src/kubernetes/api-pedido-secret.yaml
+kubectl apply -f ./src/kubernetes/api-pedido-deployment.yaml
+kubectl apply -f ./src/kubernetes/api-pedido-svc.yaml
+kubectl apply -f ./src/kubernetes/api-pedido-hpa.yaml
+
+# API Producao
+kubectl apply -f ./src/kubernetes/api-producao-secret.yaml
+kubectl apply -f ./src/kubernetes/api-producao-deployment.yaml
+kubectl apply -f ./src/kubernetes/api-producao-svc.yaml
+kubectl apply -f ./src/kubernetes/api-producao-hpa.yaml
+
+# API Producao
+kubectl apply -f ./src/kubernetes/api-pagamento-secret.yaml
+kubectl apply -f ./src/kubernetes/api-pagamento-deployment.yaml
+kubectl apply -f ./src/kubernetes/api-pagamento-svc.yaml
+kubectl apply -f ./src/kubernetes/api-pagamento-hpa.yaml
